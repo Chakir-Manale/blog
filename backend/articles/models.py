632e260daede5acc40ebefe92a00c0979edcd1e4
Chakir-manale <you@example.com>
2020-10-26 12:32:48 +0100

    my blog app

---
+++ backend/articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
